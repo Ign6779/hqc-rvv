b57574c458e79df24999e2c9b51bb886ac6e4e45
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2026-05-22 13:49:49 +0200

    basic structure

---
+++ src/riscv/rvv/gf2x.c
@@ -0,0 +1,25 @@
+ inline void toom_3_mult() {
+
+}
+
+static inline void karat_mult() {
+
+}
+
+static inline void reduce() {
+
+}
+
+
+/**
+* @brief Multiply two polynomials mod X^n - 1
+*
+* This function multiplies two polynomials a1 and a2 without considering sparsity.
+* Multiplication is done mod(X^n - 1)
+* @param[out] o Pointer to result
+* @param[in] a1 Pointer to first polynomial
+* @param[in] a2 Pointer to second polynomial
+*/
+void vect_mul(uint64_t *o, const uint64_t *a1, const uint64_t *a2) {
+
+}
